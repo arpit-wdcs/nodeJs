0f480dd93709bd513ec56813a040abad249fcee6
arpit-wdcs <97589267+arpit-wdcs@users.noreply.github.com>
2022-01-18 12:48:59 +0530

    Create expressApp.js

---
+++ expressApp.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) =>  {
+    console.log("hello arpit")
+    var Web3 = require('web3');
+    var web3 = new Web3('ws://localhost:8546');
+   var privateKey= web3.eth.accounts.create();
+    res.send(privateKey)
+
+})
+
+const port = process.env.PORT || 3000
+console.log('EllllllNV PORT', process.env.PORT)
+
+
+app.listen(port,() => console.log(`listning on port  ${port} `))
